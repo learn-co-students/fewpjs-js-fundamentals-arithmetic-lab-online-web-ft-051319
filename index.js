9ceec8f2469e934e68984b5fdc1e9fe59a018cd8
Travis Exline <t.exline22@gmail.com>
2019-08-26 16:02:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1
+
+var mod = 16
+mod %= 6
+
+var max = Math.max(1, 2, 3, 4, 20)
