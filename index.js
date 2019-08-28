c8f834e9ef7e890189152b7f4cc7d6e10cabd85d
wkpescherine <wkpescherine@gmail.com>
2019-08-28 15:18:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1*num2;
+
+const random = Math.random(100)+1;
+
+const mod =  24%5;
+
+const max = Math.max(1.3, 9,19,20);
