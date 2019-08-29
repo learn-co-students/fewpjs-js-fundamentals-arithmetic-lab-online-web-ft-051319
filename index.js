e16c2e268ae67196a9d5b17cb2169edb7243a7e4
tanfly <mcewen.stella@gmail.com>
2019-08-29 15:11:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let num = 16
 
-// Write your code here
+const multiply = (num1 * num2)
+
+const random = Math.random();
+
+const mod = (num %= 6)
+
+const max = Math.max(5, 10, 15, 20)
