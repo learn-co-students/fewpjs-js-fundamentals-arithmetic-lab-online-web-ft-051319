ac5cef8359e34245d4a7ca0d57c503ce010faa9d
Natalie <natsetc@gmail.com>
2019-09-03 00:59:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 1;
+let num2 = 62;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const mod = (12 % 8)
+
+const max = Math.max(1, 10, 20)
