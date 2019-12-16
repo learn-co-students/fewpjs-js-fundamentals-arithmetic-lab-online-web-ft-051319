0a065e8582481d800d55bf786c8df73d103c9f76
jenniferbusca <jennifer.busca@gmail.com>
2019-12-16 10:35:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var num1 = 62
 var num2 = 1
-let multiply = num1 * num2
+
+let multiply = num1 * num2;
 let random = Math.random();
 let mod = 10 % 6;
-let max = Math.max(1, 2, 3, 4, 20);
+let max = Math.max(10,20);
